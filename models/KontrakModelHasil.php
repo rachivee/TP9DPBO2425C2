72fad66773bfd1be504750caf4e7d0328e8d78eb
rachivee <farahmauliida@gmail.com>
2025-11-25 21:38:04 +0700

    Delete models directory

--- models/KontrakModelHasil.php
+++
@@ -1,9 +0,0 @@
-<?php
-interface KontrakModelHasil {
-    public function getAllHasil();
-    public function addHasil($gp, $tgl, $id_p, $pos, $poin);
-    public function getHasilById($id); // Untuk Edit
-    public function updateHasil($id, $gp, $tgl, $id_p, $pos, $poin); // Untuk Edit
-    public function deleteHasil($id); // Untuk Delete
-}
-?>
